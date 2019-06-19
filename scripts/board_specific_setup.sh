8f78a2f0c9d00d6bdc008063b198f1335352ca3e
Kamil Trzciński <ayufan@ayufan.eu>
2019-06-19 22:37:59 +0200

    Install `boot.scr` during build

--- scripts/board_specific_setup.sh
+++ scripts/board_specific_setup.sh
@@ -17,13 +17,34 @@ install_rockpro64_bootloader() {
     bs=512 \
     seek=64 || die "fail to install uboot fireware"
 
+  info "Installed bootloader."
+}
+
+install_rockpro64_efi_boot_scr() {
+  local image="$1"
+  local efi_offset_sectors=$(partoffset "$1" 12)
+  local efi_size_sectors=$(partsize "$1" 12)
+  local efi_offset=$((efi_offset_sectors * 512))
+  local efi_size=$((efi_size_sectors * 512))
+  local efi_dir=$(mktemp -d)
+
+  sudo mount -o loop,offset=${efi_offset},sizelimit=${efi_size} "$1" \
+    "${efi_dir}"
+  sudo cp "${ROOT}/boot/boot.scr" "${efi_dir}/"
+  sudo umount "${efi_dir}"
+  rmdir "${efi_dir}"
+}
+
+make_root_a_b_bootable() {
+  local image="$1"
+
   info "Make ROOT-A/ROOT-B bootable"
   cgpt add -i 3 -B 1 "$image" # ROOT-A
   cgpt add -i 5 -B 1 "$image" # ROOT-B
-
-  info "Installed bootloader."
 }
 
 board_setup() {
   install_rockpro64_bootloader "$1"
+  install_rockpro64_efi_boot_scr "$1"
+  # make_root_a_b_bootable "$1"
 }
